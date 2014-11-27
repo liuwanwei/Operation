4455a5802710723dc046463a83f410e9a394d771
liuwanwei <liuwanwei@gmail.com>
2014-11-27 17:35:33 +0800

    下雨的傍晚，冷雨，有点脚冷

---
+++ OperationLib/Operation/Operation.h
@@ -0,0 +1,13 @@
+//
+//  Operation.h
+//  Operation
+//
+//  Created by sungeo on 14/11/27.
+//  Copyright (c) 2014年 buddysoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Operation : NSObject
+
+@end
---
+++ OperationLib/Operation/Operation.m
@@ -0,0 +1,13 @@
+//
+//  Operation.m
+//  Operation
+//
+//  Created by sungeo on 14/11/27.
+//  Copyright (c) 2014年 buddysoft. All rights reserved.
+//
+
+#import "Operation.h"
+
+@implementation Operation
+
+@end
